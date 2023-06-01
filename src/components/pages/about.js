a60ba4bc72d95b19642779796038259d8f3e2aa9
Daniel Arroyo <darroyo083@gmail.com>
2023-06-02 01:34:59 +0200

    Updated Portfolio

---
+++ src/components/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react";
+//import profilePicture from "../../../static/assets/images/bio/headshot.jpg";
+
+export default function() {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        // style={{
+        //   background: "url(" + profilePicture + ") no-repeat",
+        //   backgroundSize: "cover",
+        //   backgroundPosition: "center"
+        // }}
+      />
+      <div className="right-column">
+       <p>Hello there! I'm Daniel Arroyo, a passionate web development student with a lifelong love for computers and technology. Since I was young, I knew that I wanted to pursue a career in something related to computers, and my fascination with the world of information technology has only grown over the years.
+       </p>
+       <p>Currently, I am studying web application development at Zabalburu, where I am gaining a solid foundation in various programming languages and techniques. While exploring different paths within the field, I have found a particular interest in Java. Its versatility and wide range of applications have captivated me, and I'm eager to delve deeper into this language. However, I'm also open to learning and mastering other programming languages as I believe in the importance of being adaptable and keeping up with the ever-changing industry trends.
+       </p>
+       <p>I am driven by an insatiable thirst for knowledge and growth, and I see myself continuously expanding my skills and professional development in the tech sector. I thrive on solving complex problems and finding creative solutions through code. My dedication and motivation to learn are the fuel that propels me forward, and I'm excited about the endless possibilities that lie ahead.
+       </p>
+       <p>If you have any inquiries or collaboration opportunities, don't hesitate to get in touch. Let's connect, learn, and make a positive impact together!
+       </p>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/pages/contact.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import contactPagePicture from "../../../static/assets/images/auth/login.jpg";
+
+export default function() {
+  return (
+    <div className="content-page-wrapper">
+      <div
+        className="left-column"
+        style={{
+          background: "url(" + contactPagePicture + ") no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center"
+        }}
+      />
+      <div className="right-column">
+        <div className="contact-bullet-points">
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="phone" />
+            </div>
+
+            <div className="text">###</div>
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="envelope" />
+            </div>
+
+            <div className="text">darroyo083@gmail.com</div>
+              
+          </div>
+
+          <div className="bullet-point-group">
+            <div className="icon">
+              <FontAwesomeIcon icon="map-marked-alt" />
+            </div>
+
+            <div className="text">Bilbao, Spain</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
